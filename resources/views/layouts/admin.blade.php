6f356d3ce18ba83eb116c2d5a5d0273452c4d053
Balbuena-lang <balbuenadexter2@gmai.com>
2026-05-02 22:14:06 +0800

    fix ebook uploads and errors

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -307,6 +307,15 @@
                     {{ session('error') }}
                 </div>
             @endif
+            @if($errors->any())
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-lg mb-4">
+                    <ul class="list-disc list-inside text-sm font-medium">
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
             @yield('content')
         </main>
